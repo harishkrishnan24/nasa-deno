287d027405db01250bf44f595f1b391a80fc2424
Harish krishnan <harishkrishnan1993@gmail.com>
2021-03-19 16:36:17 +0530

    First API

--- mod.ts
+++ mod.ts
@@ -3,6 +3,12 @@ import { Application } from "https://deno.land/x/oak@v6.5.0/mod.ts";
 const app = new Application();
 const PORT = 8000;
 
-app.listen({
-  port: PORT,
+app.use((ctx) => {
+  ctx.response.body = "Mission control API";
 });
+
+if (import.meta.main) {
+  await app.listen({
+    port: PORT,
+  });
+}
