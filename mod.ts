f9abc20996b32fe35eb4ac54a3e22290f1343094
Harish krishnan <harishkrishnan1993@gmail.com>
2021-03-19 19:32:39 +0530

    Add front-end code

--- mod.ts
+++ mod.ts
@@ -3,8 +3,22 @@ import { Application } from "https://deno.land/x/oak@v6.5.0/mod.ts";
 const app = new Application();
 const PORT = 8000;
 
-app.use((ctx) => {
+app.use(async (ctx, next) => {
+  await next();
+  const time = ctx.response.headers.get("X-Response-Time");
+  console.log(`${ctx.request.method} ${ctx.request.url}: ${time}`);
+});
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const delta = Date.now() - start;
+  ctx.response.headers.set("X-Response-Time", `${delta}ms`);
+});
+
+app.use(async (ctx, next) => {
   ctx.response.body = "Mission control API";
+  await next();
 });
 
 if (import.meta.main) {
