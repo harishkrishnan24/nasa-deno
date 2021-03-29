7261e699f0d904b2b2d9164fceb6698279cb3c1b
Harish krishnan <harishkrishnan1993@gmail.com>
2021-03-29 20:03:20 +0530

    planets test

--- models/planets.test.ts
+++ models/planets.test.ts
@@ -3,26 +3,47 @@ import {
   assertNotEquals,
 } from "https://deno.land/std/testing/asserts.ts";
 
-import { filterHabitablePlanets } from './planets.ts';
+import { filterHabitablePlanets } from "./planets.ts";
 
-Deno.test("Short Example test", () => {
-  assertEquals("deno", "deno");
-  assertNotEquals({ runtime: "deno" }, { runtime: "node" });
-  console.log(Deno.build.os);
-});
+const HABITABLE_PLANET = {
+  koi_disposition: "CONFIRMED",
+  koi_prad: "1",
+  koi_srad: "1",
+  koi_smass: "1",
+};
 
-Deno.test({
-  name: "ops leak",
-  sanitizeOps: false,
-  fn() {
-    setTimeout(console.log, 10000);
-  },
-});
+const NOT_CONFIRMED = {
+  koi_disposition: "FALSE POSITIVE",
+};
 
-Deno.test({
-  name: "resource leak",
-  sanitizeResources: false,
-  async fn() {
-    await Deno.open("./models/planets.ts");
-  },
+const TOO_LARGE_PLANETARY_RADIUS = {
+  koi_disposition: "CONFIRMED",
+  koi_prad: "1.5",
+  koi_srad: "1",
+  koi_smass: "1",
+};
+
+const TOO_LARGE_SOLAR_RADIUS = {
+  koi_disposition: "CONFIRMED",
+  koi_prad: "1",
+  koi_srad: "1.01",
+  koi_smass: "1",
+};
+
+const TOO_LARGE_SOLAR_MASS = {
+  koi_disposition: "CONFIRMED",
+  koi_prad: "1",
+  koi_srad: "1",
+  koi_smass: "1.04",
+};
+
+Deno.test("Filter only habitable planets", () => {
+  const filtered = filterHabitablePlanets([
+    HABITABLE_PLANET,
+    NOT_CONFIRMED,
+    TOO_LARGE_PLANETARY_RADIUS,
+    TOO_LARGE_SOLAR_MASS,
+    TOO_LARGE_SOLAR_RADIUS,
+  ]);
+  assertEquals(filtered, [HABITABLE_PLANET]);
 });
